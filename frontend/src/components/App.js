0b6e63ab8bd662409463c0c1aaa47b734e002684
Thomas Kebschull <tommykebschull@gmail.com>
2021-12-14 20:39:49 -0800

    Frontend progress wiring up state variables

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
-import { Container, Grid, Input } from "semantic-ui-react";
+import { Container, Grid, Input, Button, Icon } from "semantic-ui-react";
 import styles from '../../static/css/modular.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
+            url: '',
 
         };
     }
 
-    
+    onSubmit = () => {
+        console.log(this.state.url);
+    }
+
     render()
     {
         const { Row, Column } = Grid;
@@ -27,10 +30,16 @@ class App extends Component {
                                 <h1>Title Bot</h1>
                             </Column>
                         </Row>
-                        <Row columns={3}>
+                        <Row columns={4}>
                             <Column width={2} />
-                            <Column width={12} textAlign="center" >
-                                <Input placeholder="Input a website here..." fluid />
+                            <Column width={10} textAlign="center" >
+                                <Input placeholder="Input a website here..." fluid onChange={event => this.setState({ url: event.target.value })} />
+                            </Column>
+                            <Column width={2} textAlign="left">
+                                <Button fluid animated='vertical' color='black' size='medium' onClick={this.onSubmit}>
+                                    <Button.Content hidden><Icon name='search' /></Button.Content>
+                                    <Button.Content visible>Search Title</Button.Content>
+                                </Button>
                             </Column>
                             <Column width={2} />
                         </Row>
@@ -43,7 +52,9 @@ class App extends Component {
                                     <HomePage />
                                 </Row>
                             </Column>
-                            <Column></Column>
+                            <Column>
+                                <HomePage />
+                            </Column>
                         </Row>
                     </Grid>
                 </Container>              
